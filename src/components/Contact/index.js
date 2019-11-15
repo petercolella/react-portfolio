50d0df92fadb00c19dd8934b9d2f70ca6aef366e
petercolella <peter.joseph.colella@gmail.com>
2019-11-14 21:28:50 -0500

    adds links and svgs to Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { ReactComponent as GitHub } from './github.svg';
+import { ReactComponent as Gmail } from './gmail.svg';
+import { ReactComponent as LinkedIn } from './linkedin.svg';
 import './styles.css';
 
 const Contact = props => {
@@ -10,7 +13,26 @@ const Contact = props => {
           {char.char}
         </span>
       ))}
-      <div className={!show ? 'contact' : 'contact show'}></div>
+      <div className={!show ? 'contact' : 'contact show'}>
+        <a
+          href="https://github.com/petercolella"
+          target="_blank"
+          rel="noopener noreferrer">
+          <GitHub className={!show ? 'svg' : 'svg grow'} />
+        </a>
+        <a
+          href="mailto:colella.peter.j@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer">
+          <Gmail className={!show ? 'svg' : 'svg grow'} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/peter-colella/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <LinkedIn className={!show ? 'svg' : 'svg grow'} />
+        </a>
+      </div>
       {closingCharArr.map(char => (
         <span key={char.key} style={{ color: char.color }}>
           {char.char}
