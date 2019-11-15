a9c3a4162df62eb72ea75aa0bcd410179c65546b
petercolella <peter.joseph.colella@gmail.com>
2019-11-14 20:11:54 -0500

    adds div character rendering to Contact component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import Heading from './components/Heading';
 import Paragraph from './components/Paragraph';
+import Contact from './components/Contact';
 
 import headingChars from './data/heading.json';
 import paragraphChars from './data/paragraph.json';
+import divChars from './data/div.json';
+import closingDivChars from './data/closing-div.json';
 
 import './App.css';
+
+const max = 250;
+const min = 50;
 
 const App = () => {
   const [headingCharArr, setHeadingCharArr] = useState([]);
   const [paraCharArr, setParaCharArr] = useState([]);
-
-  const max = 400;
-  const min = 100;
+  const [divCharArr, setDivCharArr] = useState([]);
+  const [closingDivCharArr, setClosingDivCharArr] = useState([]);
+  const [show, setShow] = useState(false);
 
   const randomTimeTotalRef = useRef();
   randomTimeTotalRef.current = 0;
@@ -54,17 +60,76 @@ const App = () => {
     [renderParaSpan]
   );
 
+  const renderDivSpan = useCallback(char => {
+    const randomTime = Math.floor(Math.random() * (max - min)) + min;
+    randomTimeTotalRef.current += randomTime;
+    setTimeout(() => {
+      setDivCharArr(divCharArr => [...divCharArr, char]);
+    }, randomTimeTotalRef.current);
+  }, []);
+
+  const renderDivChars = useCallback(
+    chars => {
+      chars.forEach((char, i) => {
+        char.key = i;
+        renderDivSpan(char);
+      });
+    },
+    [renderDivSpan]
+  );
+
+  const renderClosingDivSpan = useCallback(char => {
+    const randomTime = Math.floor(Math.random() * (max - min)) + min;
+    randomTimeTotalRef.current += randomTime;
+    setTimeout(() => {
+      setClosingDivCharArr(closingDivCharArr => [...closingDivCharArr, char]);
+    }, randomTimeTotalRef.current);
+  }, []);
+
+  const renderClosingDivChars = useCallback(
+    chars => {
+      const randomTime = Math.floor(Math.random() * (max - min)) + min;
+      randomTimeTotalRef.current += randomTime;
+      chars.forEach((char, i) => {
+        char.key = i;
+        renderClosingDivSpan(char);
+      });
+    },
+    [renderClosingDivSpan]
+  );
+
+  const renderContact = useCallback(() => {
+    randomTimeTotalRef.current += 500;
+    setTimeout(() => {
+      setShow(true);
+    }, randomTimeTotalRef.current);
+  }, []);
+
   useEffect(() => {
     setTimeout(() => {
       renderHeadingChars(headingChars);
       renderParaChars(paragraphChars);
+      renderDivChars(divChars);
+      renderClosingDivChars(closingDivChars);
+      renderContact();
     }, 1000);
-  }, [renderHeadingChars, renderParaChars]);
+  }, [
+    renderHeadingChars,
+    renderParaChars,
+    renderDivChars,
+    renderClosingDivChars,
+    renderContact
+  ]);
 
   return (
     <div className="App">
       <Heading charArr={headingCharArr} />
       <Paragraph charArr={paraCharArr} />
+      <Contact
+        charArr={divCharArr}
+        closingCharArr={closingDivCharArr}
+        show={show}
+      />
     </div>
   );
 };
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import './styles.css';
 
 const Contact = props => {
-  const { show } = props;
-  console.log(show);
-  return <div className={!show ? 'contact' : 'contact show'}></div>;
+  const { charArr, closingCharArr, show } = props;
+  return (
+    <div className="where">
+      {charArr.map(char => (
+        <span key={char.key} style={{ color: char.color }}>
+          {char.char}
+        </span>
+      ))}
+      <div className={!show ? 'contact' : 'contact show'}></div>
+      {closingCharArr.map(char => (
+        <span key={char.key} style={{ color: char.color }}>
+          {char.char}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default Contact;
