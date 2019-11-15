7e40aa6f04ccbed862fe58cde4f0732f77e6ce8b
petercolella <peter.joseph.colella@gmail.com>
2019-11-14 22:03:29 -0500

    Updates

---
+++ precache-manifest.b6fbb0c05679f50a98a93137f44d5e99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8e4a68d6a5e212789c90327349d526",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "305b8391f5dfaa5089ea",
+    "url": "/react-portfolio/static/css/main.730b6ae1.chunk.css"
+  },
+  {
+    "revision": "d5db530d0aeac165b498",
+    "url": "/react-portfolio/static/js/2.d2ed693d.chunk.js"
+  },
+  {
+    "revision": "305b8391f5dfaa5089ea",
+    "url": "/react-portfolio/static/js/main.d813e294.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "ddc250127189a3b4ee4802e6c2493757",
+    "url": "/react-portfolio/static/media/github.ddc25012.svg"
+  },
+  {
+    "revision": "5842dc8d00c779b9875faac6bbd8eb71",
+    "url": "/react-portfolio/static/media/gmail.5842dc8d.svg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/react-portfolio/static/media/linkedin.4b475fb3.svg"
+  }
+]);
