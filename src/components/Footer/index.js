3777196e9ddd5fa1acc9fbaec0cee1091250eed5
petercolella <peter.joseph.colella@gmail.com>
2019-11-16 18:36:14 -0500

    adds copyright to Footer with opacity transition

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './style.css';
 
+const year = new Date().getFullYear();
+
 const Footer = props => {
   const { show } = props;
 
-  return <div className={!show ? 'footer' : 'footer show'}></div>;
+  return (
+    <div className={!show ? 'footer' : 'footer show'}>
+      <p className={!show ? 'footer-text' : 'footer-text visible'}>
+        &#169;{year} Peter Colella
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
